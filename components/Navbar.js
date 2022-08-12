d8055472161660b8cb85807e00b093f5b89bb0c2
Top <97708528+Topgramming@users.noreply.github.com>
2022-08-12 10:16:14 -0600

    added resume/cv button functionality

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,28 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Box, Container } from '@mui/material';
+import {
+  Box,
+  Container,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  ListSubheader,
+  List,
+  ListItem,
+} from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import ResumeModal from './ResumeModal';
 
 export default function Navbar() {
+  const openResume = () => window.open(pdf);
+
   return (
-    <Container sx={{ flexGrow: 0.5 }}>
-      <AppBar
-        position="static"
-        sx={{ backgroundColor: 'transparent', boxShadow: 'none' }}
-      >
-        <Toolbar sx={{ justifyContent: 'space-between', textAlign: 'left' }}>
-          <Typography variant="h6" component="div">
-            GL - Frontend Developer
-          </Typography>
-          <Button color="inherit" sx={{ backgroundColor: 'black' }}>
-            Resume / CV
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </Container>
+    <div>
+      <Container sx={{ flexGrow: 0.5 }}>
+        <AppBar
+          position="static"
+          sx={{ backgroundColor: 'transparent', boxShadow: 'none' }}
+        >
+          <Toolbar sx={{ justifyContent: 'space-between', textAlign: 'left' }}>
+            <Typography variant="h6" component="div">
+              GL - Frontend Developer
+            </Typography>
+            <Button
+              variant={'primary'}
+              href="/resume.pdf"
+              alt="alt text"
+              target="_blank"
+              rel="noopener noreferrer"
+              sx={{ backgroundColor: 'black' }}
+            >
+              Resume / CV
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Container>
+    </div>
   );
 }
--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.pdf$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
+      },
+    });
+    return config;
+  },
+};
+
+// module.exports = nextConfig;
